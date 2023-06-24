6637d6061c55b8c062468aecf6c78f294d040e7f
Nathália Brum <natybrum.97@gmail.com>
2023-06-24 04:53:36 -0300

    Retorna a versão

--- src/app.js
+++ src/app.js
@@ -11,28 +11,8 @@ let ArrayDeUsuarios = [];
 let ArrayDeTweets = [];
 
 app.get("/tweets", (req, res) => {
-    const { page } = req.query;
-  
-    if (!page) {
-      // Se a página não for fornecida, retornar os últimos 10 tweets
-      return res.status(200).send(ArrayDeTweets.slice(-10));
-    }
-  
-    const pageNumber = parseInt(page);
-  
-    if (isNaN(pageNumber) || pageNumber <= 0) {
-      // Se a página não for um número válido ou for menor ou igual a zero, retornar erro 400
-      return res.status(400).send("Dados de página inválidos. Informe uma página válida!");
-    }
-  
-    if (pageNumber === 1) {
-      res.send(ArrayDeTweets.slice(-10));
-    } else {
-      const startIndex = (-pageNumber * 10);
-      const endIndex = (-pageNumber * 10) + 8;
-      res.send(ArrayDeTweets.slice(startIndex, endIndex));
-    }
-  });
+    res.send(ArrayDeTweets.slice(-10));
+})
 
 app.get("/tweets/:username", (req, res) => {
     const { username } = req.params;
