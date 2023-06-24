a83f1972f61137776b9435fa5475ff6b28ffa135
Nathália Brum <natybrum.97@gmail.com>
2023-06-24 02:58:09 -0300

    Versão teste 3

--- src/app.js
+++ src/app.js
@@ -11,23 +11,8 @@ let ArrayDeUsuarios = [];
 let ArrayDeTweets = [];
 
 app.get("/tweets", (req, res) => {
-    const { page } = req.query;
-  
-    if (!page || page <= 0 || isNaN(page)) {
-      return res.status(400).send("Informe uma página válida!");
-    }
-  
-    const pageNumber = parseInt(page);
-  
-    if (pageNumber === 1) {
-        res.status(200).send(ArrayDeTweets.slice(-10));
-    } else {
-      const startIndex = -pageNumber * 10;
-      const endIndex = startIndex + 10;
-      res.send(ArrayDeTweets.slice(startIndex, endIndex));
-    }
-  });
-  
+    res.send(ArrayDeTweets.slice(-10));
+})
 
 app.get("/tweets/:username", (req, res) => {
     const { username } = req.params;
@@ -61,13 +46,14 @@ app.post("/tweets", (req, res) => {
         return res.status(401).send("Unauthorized");
     }
 
-    const { username, tweet } = req.body;
+    const { tweet } = req.body;
+    const { user } = req.headers;
 
-    if(!username || !tweet || typeof username !== "string" || typeof tweet !== "string"){
+    if(!user || !tweet || typeof user !== "string" || typeof tweet !== "string"){
         return res.status(400).send("Todos os campos são obrigatórios!");
     }
 
-    const novoTweet = {username, avatar: VarAvatar, tweet};
+    const novoTweet = {user, avatar: VarAvatar, tweet};
 
     ArrayDeTweets.push(novoTweet);
 
